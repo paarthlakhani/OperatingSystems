83e6bc6bad7b9801210e794a0750ca4c267e1d71
paarthlakhani <paarth.lakhani@utah.edu>
2017-03-26 17:19:44 -0600

    Started Assignment4

--- Assignment4/problem_1.c
+++ Assignment4/problem_1.c
@@ -1,13 +1,22 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<pthread.h>
 
+// command line arguments
 int n_cats = 0;
 int n_dogs = 0;
 int n_birds = 0;
 
+// condition variables
+pthread_cond_t cats;
+pthread_cond_t dogs;
+pthread_cond_t birds;
+
+// mutex lock
+pthread_mutex_t moniter_lock;
+
 int main(int argc, char *argv[])
 {
-
   if(argc !=  4)
     {
       fprintf(stderr,"Three Arguments not given");
@@ -18,7 +27,6 @@ int main(int argc, char *argv[])
   n_cats = atoi(argv[1]);
   n_dogs = atoi(argv[2]);
   n_birds = atoi(argv[3]);
-  
 
   if(n_cats < 0 || n_cats > 99 || n_dogs < 0 || n_dogs > 99 || n_birds < 0 || n_birds > 99)
     {
@@ -26,17 +34,24 @@ int main(int argc, char *argv[])
     }
   else
     {
-      // DO the monitor and other stuff here
+      // initializing the mutex lock
+      pthread_mutex_init(&moniter_lock, NULL);
+
+      // initializing the mutex lock
+      pthread_cond_init(&cats, NULL);
+      pthread_cond_init(&dogs, NULL);
+      pthread_cond_init(&birds,NULL);
+      
     }
 }
 
 void play(void) {
-  /*for (int i=0; i<10; i++) 
+  for (int i=0; i<10; i++) 
     {
     assert(cats >= 0 && cats <= n_cats);
     assert(dogs >= 0 && dogs <= n_dogs);
     assert(birds >= 0 && birds <= n_birds);
     assert(cats == 0 || dogs == 0);
     assert(cats == 0 || birds == 0);
-    }*/
+    }
 }
